a1b975f79f457aaa08b7447e96592ae505dad25d
Diego <diannewdev@gmail.com>
2024-07-26 15:40:53 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className='bg-red-500'>
+      <h1 className='text-3xl font-bold underline'>Hdello world</h1>
+    </div>
+    
+  );
+};
+
+export default App;
