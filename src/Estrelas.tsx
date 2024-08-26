47d38c7a1cbbeaac2633386b5e0719197ac0f54b
Diego <diannewdev@gmail.com>
2024-08-26 16:32:30 -0400

    resolvendo scale do titulo muito grande

--- src/Estrelas.tsx
+++ src/Estrelas.tsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Estrelas: React.FC = () => {
     return (
         <div className='flex'>
-            <img className='w-4' src="images/star.png" alt="Ícone de Estrela" />
-            <img className='w-4' src="images/star.png" alt="Ícone de Estrela" />
-            <img className='w-4' src="images/star.png" alt="Ícone de Estrela" />
-            <img className='w-4' src="images/star.png" alt="Ícone de Estrela" />
-            <img className='w-4' src="images/star.png" alt="Ícone de Estrela" />
+            <img className='w-4' src={".assets/images/star.png"} alt="Ícone de Estrela" />
+            <img className='w-4' src={".assets/images/star.png"} alt="Ícone de Estrela" />
+            <img className='w-4' src={".assets/images/star.png"} alt="Ícone de Estrela" />
+            <img className='w-4' src={".assets/images/star.png"} alt="Ícone de Estrela" />
+            <img className='w-4' src={".assets/images/star.png"} alt="Ícone de Estrela" />
         </div>
         
     );
