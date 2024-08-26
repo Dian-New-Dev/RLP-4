d2f2795d76f67d35f411d213284536232f2cc579
Diego <diannewdev@gmail.com>
2024-08-26 15:41:26 -0400

    corrigi paths de imagens para renderizar após deploy

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
     return (
         <div className='w-full bg-sky-950 text-sky-100 flex flex-row sm:flex-col text-xs font-roboto justify-around items-center gap-2'>
             <div className='sm:order-1'>
-                <img className="w-24 sm:w-32 pt-1 ml-[-10px] sm:ml-0" src="images/logo-bg2.png" alt="Logo" />
+                <img className="w-24 sm:w-32 pt-1 ml-[-10px] sm:ml-0" src={"./images/logo-bg2.png"} alt="Logo" />
             </div>
 
             <div className='sm:order-3 flex flex-col text-sky-600 text-center sm:text-left sm:flex-row gap-1 sm:gap-4 ml-[-15px]'>
@@ -20,9 +20,9 @@ const Footer: React.FC = () => {
             </div>
 
             <div className='sm:order-2 flex gap-2'>
-                <a href="www.facebook.com" target='_blank'><img className='w-4 sm:w-8 hover:border' src="images/facebook.png" alt="Ícone de Facebook" /></a>
-                <a href="www.twitter.com" target='_blank'><img className='w-4 sm:w-8 hover:border' src="images/twitter.png" alt="Ícone de Twitter" /></a>
-                <a href="www.instagram.com" target='_blank'><img className='w-4 sm:w-8 hover:border' src="images/instagram.png" alt="Ícone de Instagram" /></a>
+                <a href="www.facebook.com" target='_blank'><img className='w-4 sm:w-8 hover:border' src={"./images/facebook.png"} alt="Ícone de Facebook" /></a>
+                <a href="www.twitter.com" target='_blank'><img className='w-4 sm:w-8 hover:border' src={"./images/twitter.png"} alt="Ícone de Twitter" /></a>
+                <a href="www.instagram.com" target='_blank'><img className='w-4 sm:w-8 hover:border' src={"./images/instagram.png"} alt="Ícone de Instagram" /></a>
             </div>
         </div>
         
--- src/Sobre.tsx
+++ src/Sobre.tsx
@@ -8,32 +8,32 @@ const Sobre: React.FC = () => {
             </div>
             <div className="w-full relative flex flex-col items-center">
                 <div className="relative z-10 text-center w-3/4 max-w-[960px] h-60 mt-[-100px] bg-sky-950 flex flex-col justify-center gap-3 rounded-xl px-4 py-24">
-                    <img className="w-48 opacity-5 absolute top-5" src="images/quote.png" alt="Ícone de aspas" />
-                    <img className="w-48 opacity-5 absolute bottom-5 right-0 rotate-180" src="images/quote.png" alt="Ícone de aspas" />
+                    <img className="w-48 opacity-5 absolute top-5" src={"./images/quote.png"} alt="Ícone de aspas" />
+                    <img className="w-48 opacity-5 absolute bottom-5 right-0 rotate-180" src={"./images/quote.png"} alt="Ícone de aspas" />
                     <p className="italic text-sky-100 font-roboto">"Caminhar é a melhor forma de se exercitar, pois movimenta o corpo, aquieta a mente e reconecta a pessoa ao seu entorno natural."</p>
                     <h6 className="text-sky-100 font-roboto text-sm font-thin">Paulo Mendes Campos</h6>
                     
                 </div>
-                <div className="bg-sky-800 bg-[url('/images/about.jpg')] bg-cover bg-center h-full bg-blend  w-full mt-[-150px] pt-48 p-12 md:p-48 text-center font-roboto text-sky-100 flex flex-col items-center gap-8">
+                <div className="bg-sky-800 bg-[url('./images/about.jpg')] bg-cover bg-center h-full bg-blend  w-full mt-[-150px] pt-48 p-12 md:p-48 text-center font-roboto text-sky-100 flex flex-col items-center gap-8">
                     <h3 className="text-sky-100 font-roboto text-xl xl:text-2xl font-bold">Benefícios de uma caminhada guiada</h3>
                     <div className="grid grid-cols-2 grid-rows-2 place-items-center gap-8 2xl:gap-16 w-full max-w-[1440px] 2xl:px-28">
                         <div className="flex flex-col items-center gap-4 w-full max-w-[500px]">
-                            <img className="w-14" src="images/heart.png" alt="Ícone de coração." />
+                            <img className="w-14" src={"./images/heart.png"} alt="Ícone de coração." />
                             <h4 className="p-2 font-bold text-lg md:text-xl ">Saúde {<br />} Cardiovascular</h4>
                             <p className="border-t pt-4  text-sm md:text-base">A caminhada regular fortalece o coração, melhora a circulação sanguínea e reduz o risco de doenças cardíacas. É uma maneira simples e eficaz de manter seu sistema cardiovascular saudável e eficiente.</p>
                         </div>
                         <div className="flex flex-col items-center gap-4 w-full max-w-[500px]">
-                            <img className="w-14" src="images/weight.png" alt="Ícone de balança de peso." />
+                            <img className="w-14" src={"./images/weight.png"} alt="Ícone de balança de peso." />
                             <h4 className="p-2 font-bold text-lg md:text-xl ">Controle {<br />} de Peso</h4>
                             <p className="border-t pt-4  text-sm md:text-base">Caminhar ajuda a queimar calorias e a manter um peso saudável. É uma atividade acessível para todos os níveis de condicionamento físico e pode ser facilmente integrada à sua rotina diária.</p>
                         </div>
                         <div className="flex flex-col items-center gap-4 w-full max-w-[500px]">
-                            <img className="w-14" src="images/mental-health.png" alt="Ícone de mende sadia." />
+                            <img className="w-14" src={"./images/mental-health.png"} alt="Ícone de mende sadia." />
                             <h4 className="p-2 font-bold text-lg md:text-xl ">Saúde {<br />} Mental</h4>
                             <p className="border-t pt-4  text-sm md:text-base">Caminhar ao ar livre reduz o estresse, melhora o humor e combate a ansiedade e a depressão. A exposição à natureza e a atividade física leve liberam endorfinas, promovendo bem-estar mental.</p>
                         </div>
                         <div className="flex flex-col items-center gap-4 w-full max-w-[500px]">
-                            <img className="w-14" src="images/arm.png" alt="Ícone de músculo." />
+                            <img className="w-14" src={"./images/arm.png"} alt="Ícone de músculo." />
                             <h4 className="p-2 font-bold text-lg md:text-xl ">Fortalecimento {<br />} Muscular</h4>
                             <p className="border-t pt-4  text-sm md:text-base">A caminhada trabalha vários grupos musculares, especialmente nas pernas e no abdômen. Com o tempo, você notará melhorias em força, resistência, postura e equilíbrio, garantiando maior qualidade de vida.</p>
                         </div>
